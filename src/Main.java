559bfad21c9ac36bc133aceb8b421af2f45b21ea
Wassili Kuschnir <wassili.kuschnir@ya.ru>
2025-05-19 01:24:29 +0700

    unused imports have been deleted

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,5 @@
 import taskmanager.Managers;
 import taskmanager.TaskManager;
-import tasks.Epic;
-import tasks.Subtask;
-import tasks.Task;
-import tasks.TaskStatus;
-
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.time.Month;
 
 public class Main {
     public static void main(String[] args) {
